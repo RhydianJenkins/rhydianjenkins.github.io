1b0424abc4474391ac00effa942577a99aa44b73
Rhydian Jenkins <rhydian.jenkins@wcbs.co.uk>
2022-03-20 17:37:18 +0000

    Favicon change

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,7 +1,5 @@
 <script>
 	import { dev } from '$app/env';
-	import { page } from '$app/stores';
-	import githubLogo from '/static/github-logo.png';
 </script>
 
 <header>
@@ -13,14 +11,4 @@
 </header>
 
 <style lang="scss">
-	.corner {
-		&-left {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			position: absolute;
-			top: 0;
-			left: 0;
-		}
-	}
 </style>
