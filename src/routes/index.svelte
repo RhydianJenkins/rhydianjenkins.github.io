495d561973cd77acd1270eecaeeba8cb37180659
Rhydian Jenkins <rhydz@msn.com>
2022-03-23 23:27:31 +0000

    Color styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,19 +13,17 @@
 </script>
 
 <script>
-	const layers = [ 0, 1, 2, 3, 4, 5 ];
+	const layers = [ 0, 1, 2, 3, 4 ];
 	const layerColors = [
-		'#918D8C',
-		'#6C6868',
-		'#474444',
-		'#343232',
-		'#2B2929',
-		'#211F1F',
+		'#E1FCFD',
+		'#9CF6FB',
+		'#9DAAF2',
+		'#394F8A',
+		'#1A2238', // needs to be primary color to blend with page
 	]
 	const layerPaths = [
 		'M0,192L48,181.3C96,171,192,149,288,160C384,171,480,213,576,208C672,203,768,149,864,112C960,75,1056,53,1152,48C1248,43,1344,53,1392,58.7L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
 		'M0,128L48,106.7C96,85,192,43,288,74.7C384,107,480,213,576,224C672,235,768,149,864,106.7C960,64,1056,64,1152,101.3C1248,139,1344,213,1392,250.7L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
-		'M0,32L48,69.3C96,107,192,181,288,181.3C384,181,480,107,576,96C672,85,768,139,864,144C960,149,1056,107,1152,122.7C1248,139,1344,213,1392,250.7L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
 		'M0,96L48,128C96,160,192,224,288,208C384,192,480,96,576,101.3C672,107,768,213,864,224C960,235,1056,149,1152,122.7C1248,96,1344,128,1392,144L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
 		'M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
 		'M0,224L48,202.7C96,181,192,139,288,144C384,149,480,203,576,208C672,213,768,171,864,181.3C960,192,1056,256,1152,256C1248,256,1344,192,1392,160L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z',
@@ -51,7 +49,6 @@
 <svelte:window bind:scrollY={y}/>
 
 <div class="splash-container">
-	<img class="splash-image" src="/img/me_background.jpg" alt="Rhydian Jenkins">
 	<div class="splash">
 		<h1>Rhydian Jenkins</h1>
 		<h2>Full Stack Software Engineer</h2>
@@ -74,15 +71,7 @@
 			preserveAspectRatio="none"
 			shape-rendering="auto"
 		>
-			<linearGradient id="grad{layer}" x1="0%" y1="0%" x2="0" y2="100%">
-				<stop offset="0%" style="stop-color: {layerColors[layer]}; stop-opacity: 1" />
-				<stop offset="100%" style="stop-color: rgba(0, 0, 0, 0.1); stop-opacity: 1" />
-			</linearGradient>
-			{#if layer < layers.length - 1}
-				<path fill="url(#grad{layer})" d="{ layerPaths[layer] }" />
-			{:else}
-				<path fill="{ layerColors[layer] }" d="{ layerPaths[layer] }" />
-			{/if}
+			<path fill="{ layerColors[layer] }" d="{ layerPaths[layer] }" />
 		</svg>
 	{/each}
 
@@ -128,7 +117,7 @@
 		right: 0;
 		height: 100vh;
 		z-index: -1;
-		background-color: var(--secondary-color);
+		background-color: var(--tertiary-color);
 
 		&::before {
 			content: '';
@@ -138,7 +127,7 @@
 			left: 0;
 			right: 0;
 			z-index: 1;
-			box-shadow: inset 0 0 50rem 20rem rgba(0, 0, 0, 0.5);
+			box-shadow: inset 0 0 50rem 20rem rgba(0, 0, 0, 0.15);
 		}
 
 		.splash-image {
@@ -147,15 +136,20 @@
 			width: 100%;
 			height: 250%;
 			object-fit: cover;
+			background-color: #E1FCFD;
 
 			filter:
 				blur(4px)
-				brightness(40%)
-				contrast(50%)
-				grayscale(30%);
+				brightness(80%)
+				contrast(50%);
 		}
 
 		.splash {
+			&-wave {
+				position: fixed;
+				width: 100%;
+			}
+
 			h1,
 			h2,
 			ul {
